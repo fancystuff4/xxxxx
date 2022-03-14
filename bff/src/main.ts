a5b6194c6e480e348b55b754db2f59b2504b81e7
Atikur Rahman <atikur.rahman@borgos.in>
2022-03-14 11:52:26 +0530

    Resolved Merge Conflicts

--- bff/src/main.ts
+++ bff/src/main.ts
@@ -1,10 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Bff Apis')
+    .setDescription('Bff Api Description')
+    .setVersion('1.0')
+    .addTag('bff')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(8000);
 }
 bootstrap();
