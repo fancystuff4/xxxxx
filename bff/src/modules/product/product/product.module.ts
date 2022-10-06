a484b545f39a51759401b17fe8276c55ca636a4b
Sanjay Barman <sanjay.barman@borgos.in>
2022-10-06 17:07:46 +0530

    Product add image api implemented

--- bff/src/modules/product/product/product.module.ts
+++ bff/src/modules/product/product/product.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationService } from 'src/modules/authentication/authentication.service';
+import { FilesService } from 'src/modules/files/files.service';
 import ProductController from './product.controller';
 import { ProductService } from './product.service';
 
 @Module({
-    controllers: [ProductController],
-    providers: [ProductService, AuthenticationService],
-    exports: [ProductService]
+  controllers: [ProductController],
+  providers: [ProductService, AuthenticationService, FilesService],
+  exports: [ProductService],
 })
-
-export class ProductModule{};
+export class ProductModule {}
